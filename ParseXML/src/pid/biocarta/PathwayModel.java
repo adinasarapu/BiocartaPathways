0e45d1543b9ddcd110e0b9cc459349e6b62e6adc
adinasarapu <adinasarapu@FCGR-280-W5.ad.ufl.edu>
2015-02-11 16:18:22 -0500

    again author name added

--- ParseXML/src/pid/biocarta/PathwayModel.java
+++ ParseXML/src/pid/biocarta/PathwayModel.java
@@ -10,6 +10,10 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
+/*
+ * author: Ashok R. Dinasarapu
+ * 
+ */
 public class PathwayModel {
 	// private NodeList nl_paths;
 	private static Map<String, Interaction> interaction_list;
